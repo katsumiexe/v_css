8738c261be601d5475f07de25b7059ae0386f35b
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2020-04-23 17:36:39 +0900

    ar

---
+++ eve_2005.js
@@ -0,0 +1,13 @@
+$(function(){ 
+	$('.flex_box').hide();
+	$('.group0').show();
+
+    $('.page').on('click',function(){
+		$('.page').css('background','linear-gradient(#d0d0d0,#c0c0c0)');
+		$(this).css('background','linear-gradient(#c0a000,#a09000)');
+
+		Tmp =$(this).attr('id').replace("p", "");
+		$('.flex_box').hide();
+		$('.group'+Tmp).dadeIn(500);
+	});
+});
