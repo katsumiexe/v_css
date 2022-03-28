5cfdbafaa7a2c1485c93cd519cfea22018adf83b
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2022-03-28 11:56:33 +0900

    ar

---
+++ eve_2204.js
@@ -0,0 +1,20 @@
+$(function(){ 
+	$(".done2").on("click",function(){
+		$(".back").fadeIn(0).delay(1000).fadeOut(400);
+		$(".back_pop").animate({"top":"110vh"},0).animate({"top":"40vh"},200).animate({"top":"50vh"},50).delay(500).animate({"top":"60vh"},50).animate({"top":"-30vh"},200);
+		Tmp=$(this).attr("id").replace("dn","");
+		$(this).removeClass("done2").addClass("done3");
+		$(this).next(".box_5_pts_a").text("éÛéÊçœÇ≈Ç∑");
+
+		$.ajax({
+			type:"post",
+			url:"post_eve_2204.php",
+			data:{
+				'ope_id':Tmp,
+				'user_id':'<?=$idcode?>',
+			},
+		}).done(function(data, textStatus, jqXHR){
+			console.log(data)
+		});
+	});
+});
