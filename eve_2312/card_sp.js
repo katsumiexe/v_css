1a6f64074b1e25f2cdd21fec1bff720ba938f23a
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2023-11-28 14:53:44 +0900

    ar

--- eve_2312/card_sp.js
+++ eve_2312/card_sp.js
@@ -1,38 +1,39 @@
 $(function(){ 
+	$('#set_btn').on("click",function(){
+		$('#c3').delay(0).animate({"left":"53vw"}, 400).delay(2000).animate({"left":"28.5vw"}, 400)
+		.delay(500)
+		.animate({"left":"20vw"},250, function(){$(this).css({"z-index": "38"})}).delay(50).animate({"left":"37vw"},250).delay(50)
+		.animate({"left":"20vw"},250, function(){$(this).css({"z-index": "36"})}).delay(50).animate({"left":"37vw"},250).delay(50)
+		.animate({"left":"20vw"},250, function(){$(this).css({"z-index": "34"})}).delay(50).animate({"left":"37vw"},250).delay(50)
+		.animate({"left":"20vw"},250, function(){$(this).css({"z-index": "32"})}).delay(50).animate({"left":"37vw"},250).delay(50)
+		.animate({"left":"28.5vw"},250)
+		.delay(800)
+		.animate({"left":"53vw"}, 400)
 
-$(".chu_card").queue(function(next){
+		$('#c2').delay(100).animate({"left":"37vw"}, 400).delay(1900).animate({"left":"28.5vw"}, 400).delay(4000).animate({"left":"37vw"}, 400);
+		$('#c1').delay(200).animate({"left":"21vw"}, 400).delay(1800).animate({"left":"28.5vw"}, 400)
+		.delay(1000)
+		.animate({"left":"37vw"},250, function(){$(this).css({"z-index": "37"})}).delay(50).animate({"left":"20vw"},250).delay(50)
+		.animate({"left":"37vw"},250, function(){$(this).css({"z-index": "35"})}).delay(50).animate({"left":"20vw"},250).delay(50)
+		.animate({"left":"37vw"},250, function(){$(this).css({"z-index": "33"})}).delay(50).animate({"left":"20vw"},250).delay(50)
+		.animate({"left":"37vw"},250, function(){$(this).css({"z-index": "31"})}).delay(50).animate({"left":"20vw"},250).delay(50)
+		.animate({"left":"28.5vw"},250)
+		.delay(300)
+		.animate({"left":"21vw"}, 400)
 
-    $('#c3').delay(0).animate({"left":"53vw"}, 400).delay(3000).animate({"left":"32vw"}, 400).delay(200)
-    .animate({"left":"-6vw"}, 150).delay(50).animate({"z-index":"14"}, 0).animate({"left":"5vw"}, 150).delay(350)
-    .animate({"left":"-6vw"}, 150).delay(50).animate({"z-index":"16"}, 0).animate({"left":"5vw"}, 150).delay(350)
-    .animate({"left":"-6vw"}, 150).delay(50).animate({"z-index":"18"}, 0).animate({"left":"5vw"}, 150).delay(750)
-	.animate({"left":"53vw"}, 400)
+		$('#c0').delay(300).animate({"left":"5vw"}, 400).delay(1700).animate({"left":"28.5vw"}, 400).delay(4000).animate({"left":"5vw"}, 400);
+	    $('.box_block').fadeOut(8000);
 
-	$('#c2').delay(100).animate({"left":"37vw"}, 400).delay(2800).animate({"left":"32vw"}, 400).delay(600)
-    .animate({"left":"14vw"}, 150).delay(50).animate({"z-index":"15"}, 0).animate({"left":"5vw"}, 150).delay(350)
-    .animate({"left":"14vw"}, 150).delay(50).animate({"z-index":"17"}, 0).animate({"left":"5vw"}, 150).delay(350)
-    .animate({"left":"14vw"}, 150).delay(50).animate({"z-index":"19"}, 0).animate({"left":"5vw"}, 150).delay(350)
-	.animate({"left":"37vw"}, 400)
-
-    $('#c0').delay(3400).animate({"left":"32vw"}, 400).delay(200).animate({"left":"5vw"}, 400);
-
-    $('#c1').delay(200).animate({"left":"21vw"}, 400,function() {
-        next();
-    }).delay(2600).animate({"left":"32vw"}, 400).delay(3000).animate({"left":"21vw"}, 400);
-    $('.box_block').delay(6000).fadeOut(0);
+		setTimeout(function(){
+			$('.chu_card_b').css('transform','rotateY(-180deg)');
+			$('.chu_card_f').css('transform','rotateY(0)');
+		},1400);
+	});
+	$(".chu_card").on("click",function(){
+	    $('.box_block').show();
+		$(this).children('.chu_card_f').css('transform','rotateY(-180deg)');
+		$(this).children('.chu_card_b').css({'transform':'rotateY(0)','background-image':'url("https://katsumiexe.github.io/v_css/eve_2312/img/atari.jpg")'});
+	});
 });
-
-$(".chu_card").delay(1500).queue(function(next){
-	$('.chu_card_b').css('transform','rotateY(-180deg)');
-	$('.chu_card_f').css('transform','rotateY(0)');
-});
-
-$(".chu_card").on("click",function(){
-    $('.box_block').show();
-	$(this).children('.chu_card_f').css('transform','rotateY(-180deg)');
-	$(this).children('.chu_card_b').css({'transform':'rotateY(0)','background-image':'url("https://katsumiexe.github.io/v_css/eve_2312/img/atari.jpg")'});
-});
-});
-
 
 
