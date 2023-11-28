c53844ec8962af97caa14cc94d835e0b94023305
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2023-11-28 15:23:55 +0900

    ar

--- eve_2312/card_sp.js
+++ eve_2312/card_sp.js
@@ -33,28 +33,28 @@ $(function(){
 		$('.chu_card_b').css('transform','rotateY(-180deg)');
 		$('.chu_card_f').css('transform','rotateY(0)');
 
-		$('#c3').delay(800).animate({"left":"28.5vw"}, 400)
+		$('#c3').delay(800).animate({"left":"38vw"}, 400)
 		.delay(500)
-		.animate({"left":"20vw"},250, function(){$(this).css({"z-index": "38"})}).delay(50).animate({"left":"37vw"},250).delay(50)
-		.animate({"left":"20vw"},250, function(){$(this).css({"z-index": "36"})}).delay(50).animate({"left":"37vw"},250).delay(50)
-		.animate({"left":"20vw"},250, function(){$(this).css({"z-index": "34"})}).delay(50).animate({"left":"37vw"},250).delay(50)
-		.animate({"left":"20vw"},250, function(){$(this).css({"z-index": "32"})}).delay(50).animate({"left":"37vw"},250).delay(50)
-		.animate({"left":"28.5vw"},250)
+		.animate({"left":"20vw"},250, function(){$(this).css({"z-index": "38"})}).delay(50).animate({"left":"56vw"},250).delay(50)
+		.animate({"left":"20vw"},250, function(){$(this).css({"z-index": "36"})}).delay(50).animate({"left":"56vw"},250).delay(50)
+		.animate({"left":"20vw"},250, function(){$(this).css({"z-index": "34"})}).delay(50).animate({"left":"56vw"},250).delay(50)
+		.animate({"left":"20vw"},250, function(){$(this).css({"z-index": "32"})}).delay(50).animate({"left":"56vw"},250).delay(50)
+		.animate({"left":"38vw"},250)
 		.delay(800)
-		.animate({"left":"53vw"}, 400)
+		.animate({"left":"72.5vw"}, 400)
 
-		$('#c2').delay(800).animate({"left":"28.5vw"}, 400).delay(4000).animate({"left":"37vw"}, 400);
-		$('#c1').delay(800).animate({"left":"28.5vw"}, 400)
+		$('#c2').delay(800).animate({"left":"38vw"}, 400).delay(4000).animate({"left":"49.5vw"}, 400);
+		$('#c1').delay(800).animate({"left":"38vw"}, 400)
 		.delay(1000)
-		.animate({"left":"37vw"},250, function(){$(this).css({"z-index": "37"})}).delay(50).animate({"left":"20vw"},250).delay(50)
-		.animate({"left":"37vw"},250, function(){$(this).css({"z-index": "35"})}).delay(50).animate({"left":"20vw"},250).delay(50)
-		.animate({"left":"37vw"},250, function(){$(this).css({"z-index": "33"})}).delay(50).animate({"left":"20vw"},250).delay(50)
-		.animate({"left":"37vw"},250, function(){$(this).css({"z-index": "31"})}).delay(50).animate({"left":"20vw"},250).delay(50)
-		.animate({"left":"28.5vw"},250)
+		.animate({"left":"56vw"},250, function(){$(this).css({"z-index": "37"})}).delay(50).animate({"left":"20vw"},250).delay(50)
+		.animate({"left":"56vw"},250, function(){$(this).css({"z-index": "35"})}).delay(50).animate({"left":"20vw"},250).delay(50)
+		.animate({"left":"56vw"},250, function(){$(this).css({"z-index": "33"})}).delay(50).animate({"left":"20vw"},250).delay(50)
+		.animate({"left":"56vw"},250, function(){$(this).css({"z-index": "31"})}).delay(50).animate({"left":"20vw"},250).delay(50)
+		.animate({"left":"38vw"},250)
 		.delay(300)
-		.animate({"left":"21vw"}, 400)
+		.animate({"left":"26.5vw"}, 400)
 
-		$('#c0').delay(800).animate({"left":"28.5vw"}, 400).delay(4000).animate({"left":"5vw"}, 400);
+		$('#c0').delay(800).animate({"left":"38vw"}, 400).delay(4000).animate({"left":"3.5vw"}, 400);
 	    $('.box_block').fadeOut(7000);
 
 
