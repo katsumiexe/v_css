713a5b08ac37f7f19cb2f3d61315630f871524e0
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2023-11-28 13:02:42 +0900

    ar

--- eve_2312/card_sp.js
+++ eve_2312/card_sp.js
@@ -1,12 +1,13 @@
 $(function(){ 
 
-
 $(".chu_card").queue(function(next){
+
     $('#c3').delay(0).animate({"left":"53vw"}, 400).delay(3000).animate({"left":"32vw"}, 400).delay(200)
     .animate({"left":"-6vw"}, 150).delay(50).animate({"z-index":"14"}, 0).animate({"left":"5vw"}, 150).delay(350)
     .animate({"left":"-6vw"}, 150).delay(50).animate({"z-index":"16"}, 0).animate({"left":"5vw"}, 150).delay(350)
     .animate({"left":"-6vw"}, 150).delay(50).animate({"z-index":"18"}, 0).animate({"left":"5vw"}, 150).delay(750)
 	.animate({"left":"53vw"}, 400)
+
 	$('#c2').delay(100).animate({"left":"37vw"}, 400).delay(2800).animate({"left":"32vw"}, 400).delay(600)
     .animate({"left":"14vw"}, 150).delay(50).animate({"z-index":"15"}, 0).animate({"left":"5vw"}, 150).delay(350)
     .animate({"left":"14vw"}, 150).delay(50).animate({"z-index":"17"}, 0).animate({"left":"5vw"}, 150).delay(350)
@@ -14,6 +15,7 @@ $(".chu_card").queue(function(next){
 	.animate({"left":"37vw"}, 400)
 
     $('#c0').delay(3400).animate({"left":"32vw"}, 400).delay(200).animate({"left":"5vw"}, 400);
+
     $('#c1').delay(200).animate({"left":"21vw"}, 400,function() {
         next();
     }).delay(2600).animate({"left":"32vw"}, 400).delay(3000).animate({"left":"21vw"}, 400);
