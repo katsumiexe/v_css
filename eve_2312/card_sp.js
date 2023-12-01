1c1f989935979d8ea22b5e267c102f910d560e4f
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2023-12-01 14:06:51 +0900

    ar

--- eve_2312/card_sp.js
+++ eve_2312/card_sp.js
@@ -1,34 +1,5 @@
 $(function(){ 
 	$('#set_btn').on("click",function(){
-/*
-		$('#c3').delay(0).animate({"left":"53vw"}, 400).delay(2000).animate({"left":"28.5vw"}, 400)
-		.delay(500)
-		.animate({"left":"20vw"},250, function(){$(this).css({"z-index": "38"})}).delay(50).animate({"left":"37vw"},250).delay(50)
-		.animate({"left":"20vw"},250, function(){$(this).css({"z-index": "36"})}).delay(50).animate({"left":"37vw"},250).delay(50)
-		.animate({"left":"20vw"},250, function(){$(this).css({"z-index": "34"})}).delay(50).animate({"left":"37vw"},250).delay(50)
-		.animate({"left":"20vw"},250, function(){$(this).css({"z-index": "32"})}).delay(50).animate({"left":"37vw"},250).delay(50)
-		.animate({"left":"28.5vw"},250)
-		.delay(800)
-		.animate({"left":"53vw"}, 400)
-
-		$('#c2').delay(100).animate({"left":"37vw"}, 400).delay(1900).animate({"left":"28.5vw"}, 400).delay(4000).animate({"left":"37vw"}, 400);
-		$('#c1').delay(200).animate({"left":"21vw"}, 400).delay(1800).animate({"left":"28.5vw"}, 400)
-		.delay(1000)
-		.animate({"left":"37vw"},250, function(){$(this).css({"z-index": "37"})}).delay(50).animate({"left":"20vw"},250).delay(50)
-		.animate({"left":"37vw"},250, function(){$(this).css({"z-index": "35"})}).delay(50).animate({"left":"20vw"},250).delay(50)
-		.animate({"left":"37vw"},250, function(){$(this).css({"z-index": "33"})}).delay(50).animate({"left":"20vw"},250).delay(50)
-		.animate({"left":"37vw"},250, function(){$(this).css({"z-index": "31"})}).delay(50).animate({"left":"20vw"},250).delay(50)
-		.animate({"left":"28.5vw"},250)
-		.delay(300)
-		.animate({"left":"21vw"}, 400)
-
-		$('#c0').delay(300).animate({"left":"5vw"}, 400).delay(1700).animate({"left":"28.5vw"}, 400).delay(4000).animate({"left":"5vw"}, 400);
-	    $('.box_block').fadeOut(8000);
-		setTimeout(function(){
-			$('.chu_card_b').css('transform','rotateY(-180deg)');
-			$('.chu_card_f').css('transform','rotateY(0)');
-		},0);
-*/
 		$('#msg1').slideUp(500);
 		$('#msg2').delay(800).slideDown(500).delay3300).slideDown(500);
 
